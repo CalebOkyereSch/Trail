442049f96721874a7c3eeda5e3216e0f0d0f4fcb
Codex-C <calebokyere1@gmail.com>
2019-12-21 00:59:06 +0000

    QuizApp v1.0

---
+++ index.js
@@ -0,0 +1,314 @@
+let Quest=[
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?",
+"correct_answer":"Apple",
+"incorrect_answers":["Microsoft","Atari","Commodore"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"In any programming language, what is the most common way to iterate through an array?",
+"correct_answer":"&#039;For&#039; loops",
+"incorrect_answers":["&#039;If&#039; Statements","&#039;Do-while&#039; loops","&#039;While&#039; loops"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What does CPU stand for?",
+"correct_answer":"Central Processing Unit",
+"incorrect_answers":["Central Process Unit","Computer Personal Unit","Central Processor Unit"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Which internet company began life as an online bookstore called &#039;Cadabra&#039;?",
+"correct_answer":"Amazon ",
+"incorrect_answers": ["eBay","Overstock","Shopify"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What does the &quot;MP&quot; stand for in MP3?",
+"correct_answer":"Moving Picture",
+"incorrect_answers":["Music Player","Multi Pass","Micro Point"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"When Gmail first launched, how much storage did it provide for your email?",
+"correct_answer":"1GB",
+"incorrect_answers":["512MB","5GB","Unlimited"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Moore&#039;s law originally stated that the number of transistors on a microprocessor chip would double every...",
+"correct_answer":"Year",
+"incorrect_answers":["Four Years","Two Years","Eight Years"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What does GHz stand for?",
+"correct_answer":"Gigahertz",
+"incorrect_answers":["Gigahotz","Gigahetz","Gigahatz"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple","difficulty":"easy",
+"question":"HTML is what type of language?",
+"correct_answer":"Markup Language",
+"incorrect_answers":["Macro Language","Programming Language","Scripting Language"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"Which computer hardware device provides an interface for all other connected devices to communicate?",
+"correct_answer":"Motherboard",
+"incorrect_answers":["Central Processing Unit","Hard Disk Drive","Random Access Memory"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Which of the following languages is used as a scripting language in the Unity 3D game engine?",
+"correct_answer":"C#",
+"incorrect_answers":["Java","C++","Objective-C"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What does the Prt Sc button do?",
+"correct_answer":"Captures what&#039;s on the screen and copies it to your clipboard",
+"incorrect_answers": ["Nothing","Saves a.png file of what&#039;s on the screen in your screenshots folder in photos","Closes all windows"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"All of the following programs are classified as raster graphics editors EXCEPT:",
+"correct_answer":"Inkscape",
+"incorrect_answers":["Paint.NET","GIMP","Adobe Photoshop"]
+},
+{"category":"Science: Computers",
+"type":"multiple","difficulty":"easy",
+"question":"The series of the Intel HD graphics generation succeeding that of the 5000 and 6000 series (Broadwell) is called:",
+"correct_answer":"HD Graphics 500",
+"incorrect_answers":["HD Graphics 700 ","HD Graphics 600","HD Graphics 7000"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"What did the name of the Tor Anonymity Network orignially stand for?",
+"correct_answer":"The Onion Router",
+"incorrect_answers":["The Only Router","The Orange Router","The Ominous Router"]
+},
+{"category":"Science: Computers",
+"type":"multiple","difficulty":"medium",
+"question":"What was the first commerically available computer processor?",
+"correct_answer":"Intel 4004",
+"incorrect_answers":["Intel 486SX","TMS 1000","AMD AM386"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"In web design, what does CSS stand for?",
+"correct_answer":"Cascading Style Sheet",
+"incorrect_answers":["Counter Strike: Source","Corrective Style Sheet","Computer Style Sheet"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What is the code name for the mobile operating system Android 7.0?",
+"correct_answer":"Nougat",
+"incorrect_answers":["Ice Cream Sandwich","Jelly Bean","Marshmallow"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"What is the domain name for the country Tuvalu?",
+"correct_answer":".tv",
+"incorrect_answers":[".tu",".tt",".tl"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"While Apple was formed in California, in which western state was Microsoft founded?",
+"correct_answer":"New Mexico",
+"incorrect_answers": ["Washington","Colorado","Arizona"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"What does the acronym CDN stand for in terms of networking?",
+"correct_answer":"Content Delivery Network",
+"incorrect_answers": ["Content Distribution Network","Computational Data Network","Compressed Data Network"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple","difficulty":"medium",
+"question":"In the server hosting industry IaaS stands for...",
+"correct_answer":"Infrastructure as a Service",
+"incorrect_answers":["Internet as a Service","Internet and a Server","Infrastructure as a Server"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"On Twitter, what is the character limit for a Tweet?",
+"correct_answer":"140",
+"incorrect_answers":["120","160","100"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"hard",
+"question":"What port does HTTP run on?","correct_answer":"80",
+"incorrect_answers" :["53","443","23"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"In the programming language &quot;Python&quot;, which of these statements would display the string &quot;Hello World&quot; correctly?",
+"correct_answer":"print(&quot;Hell o World&quot;)",
+"incorrect_answers": ["console.log(&quot;Hello World&quot;)","echo &quot;Hello World&quot;","printf(&quot;Hello World&quot;)"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"In programming, the ternary operator is mostly defined with what symbol(s)?",
+"correct_answer":"?:",
+"incorrect_answers":["??","if then","?"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Which of the following is a personal computer made by the Japanese company Fujitsu?",
+"correct_answer":"FM7",
+"incorrect_answers":["PC-9801","Xmillennium ","MSX"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"The numbering system with a radix of 16 is more commonly referred to as ",
+"correct_answer":"Hexidecimal",
+"incorrect_answers":["Binary","Duodecimal","Octal"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"hard",
+"question":"Which data structure does FILO apply to?",
+"correct_answer":"Stack","incorrect_answers":["Queue","Heap","Tree"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"hard",
+"question":"Which of these is not a key value of Agile software development?",
+"correct_answer":"Comprehensive documentation",
+"incorrect_answers": ["Individuals and interactions","Customer collaboration","Responding to change"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"This mobile OS held the largest market share in 2012.",
+"correct_answer":"iOS",
+"incorrect_answers":["Android","BlackBerry","Symbian"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Which of these people was NOT a founder of Apple Inc?",
+"correct_answer":"Jonathan Ive",
+"incorrect_answers":["Steve Jobs","Ronald Wayne","Steve Wozniak"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"hard",
+"question":"Which RAID array type is associated with data mirroring?",
+"correct_answer":"RAID 1",
+"incorrect_answers":["RAID 0","RAID 10","RAID 5"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"medium",
+"question":"Laserjet and inkjet printers are both examples of what type of printer?",
+"correct_answer":"Non-impact printer",
+"incorrect_answers":["Impact printer","Daisywheel printer","Dot matrix printer"]
+},
+{"category":"Science: Computers",
+"type":"multiple","difficulty":"hard",
+"question":"What is the name given to layer 4 of the Open Systems Interconnection (ISO) model?",
+"correct_answer":"Transport",
+"incorrect_answers":["Session","Data link","Network"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"hard",
+"question":"Who invented the &quot;Spanning Tree Protocol&quot;?",
+"correct_answer":"Radia Perlman",
+"incorrect_answers":["Paul Vixie","Vint Cerf","Michael Roberts"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"Which programming language shares its name with an island in Indonesia?",
+"correct_answer":"Java",
+"incorrect_answers":["Python","C","Jakarta"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"How long is an IPv6 address?",
+"correct_answer":"128 bits",
+"incorrect_answers":["32 bits","64 bits","128 bytes"]
+},
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"In computing, what does MIDI stand for?",
+"correct_answer":"Musical Instrument Digital Interface",
+"incorrect_answers":["Musical Interface of Digital Instruments","Modular Interface of Digital Instruments","Musical Instrument Data Interface"]
+}, 
+{"category":"Science: Computers",
+"type":"multiple",
+"difficulty":"easy",
+"question":"In computing, what does LAN stand for?",
+"correct_answer":"Local Area Network",
+"incorrect_answers":["Long Antenna Node","Light Access Node","Land Address Navigation"]
+}
+];
+
+
+ let questions= document.getElementById('questions');
+ let answers = document.getElementById('answers');  
+ let score = document.getElementById('score');
+ let meg = document.getElementById('meg');
+ let item0 = document.getElementById('item0');
+ let item1 = document.getElementById('item1');
+ let item2 = document.getElementById('item2');
+ let item3 = document.getElementById('item3');
+ let array =[item0,item2,item1,item3];
+ let eve = 0;
+ let ank; 
+ let us = 0;
+ function ansa(value){
+ 	let ans = Quest[value].incorrect_answers;
+ 	ans.push(Quest[value].correct_answer);
+
+ 	return ans;
+
+ }
+ 
+ while(eve > -1){
+
+ 	let rand =Math.floor( Math.random()*Quest.length);
+ 	let mem =Math.floor( Math.random()*4);
+ 	score.innerHTML= `${us}`;
+
+	questions.innerHTML = Quest[rand].question;
+	ank= ansa(rand);
+	for(let i=0;i<4;i++){
+		array[i].innerHTML=ank[i];
+	}
+	
+
+   setTimeOut(1000,()=>{
+   		meg.innerHTML="next";
+   })
+}
